9c6d2d18c83e52246743c35534403de462c7d3ed
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-07-20 22:22:56 +0700

    feat: add database connection utility with environment variable loading

---
+++ internal/lib/conn.go
@@ -0,0 +1,31 @@
+package lib
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func Conn() (*pgxpool.Pool, error) {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Cannot read file ekstensions .env")
+	}
+
+	getConn := os.Getenv("DATABASE_URL")
+
+	pool, err := pgxpool.New(context.Background(), getConn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(context.Background()); err != nil{
+		pool.Close()
+		return nil, err
+	}
+
+	return pool, nil
+}
